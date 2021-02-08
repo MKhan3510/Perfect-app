49d0342be7d21ac07d3c78ee31a19093d8f223b0
mkhan3510 <mkhan3510@richmondhillhs.org>
2021-02-08 15:12:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Components/navbar'
+import Dashboard from './Components/dashboard'
+import Card from './Components/card'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar>
+      <Dashboard>
+          <div className="card-container">
+      <Card>
+      <Card>
+    </div>
+    </div>
+  );
+}
+
+export default App;
